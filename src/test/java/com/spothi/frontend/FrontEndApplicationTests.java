4a2bb019e043cad64aed3d5f8983ebc4a3bdc43a
Sudershan Pothina <sudershanpothina@gmail.com>
2021-07-24 11:36:59 -0500

    initial commit

---
+++ src/test/java/com/spothi/frontend/FrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spothi.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
